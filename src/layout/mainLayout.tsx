a3aa388b3e8e933f53d23ce0199c1e230f5c1c92
suryakant thombare <suryakant.thombare@ispace.com>
2023-04-07 10:44:40 +0530

    MainLayout refactor and ts error resolved

--- src/layout/mainLayout.tsx
+++ src/layout/mainLayout.tsx
@@ -1,12 +1,22 @@
 import Navbar from "@/components/header";
 import { Footer } from "@/components/footer";
+import Paper from "@mui/material/Paper";
 
-export default function MainLayout({ children }) {
-	return (
-		<div>
+export default function MainLayout(props: { children?: any }) {
+    return (
+        <div>
             <Navbar />
-            {children}
+            <Paper
+                sx={{
+                    borderRadius: "unset",
+                    boxShadow: "none",
+                    backgroundColor: "#fafafa",
+                    minHeight: "calc(100vh - 4rem)",
+                }}
+            >
+                {props?.children}
+            </Paper>
             <Footer />
-		</div>
-	);
+        </div>
+    );
 }
